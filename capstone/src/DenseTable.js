ebf2043cd47c68bdbd2869208738973a65a7b9d1
Faper <43310141+Seo-Faper@users.noreply.github.com>
2022-12-05 15:30:53 +0000

    Fast API <-> React 연결 완료

--- capstone/src/DenseTable.js
+++ capstone/src/DenseTable.js
@@ -1,41 +1,43 @@
-import * as React from "react";
+import React, { useState } from "react";
 import Button from "@mui/material/Button";
 import Badge from "@mui/material/Badge";
 import Stack from "@mui/material/Stack";
 import SnackbarContent from "@mui/material/SnackbarContent";
 import { ButtonGroup } from "@mui/material";
 
-const action = (
-  <Stack spacing={2} direction="row">
-    <ButtonGroup>
-      <Button variant="contained" color="success">
-        C
-      </Button>
-      <Button variant="text" color="success">
-        0
-      </Button>
-    </ButtonGroup>
-    <ButtonGroup>
-      <Button variant="contained">D</Button>
-      <Button variant="text">0</Button>
-    </ButtonGroup>
-  </Stack>
-);
-
 export default function DenseTable() {
-  return (
-    <Stack spacing={2.3}>
-      <SnackbarContent message="YYYY-MM-DD" action={action} />
-      <SnackbarContent message="YYYY-MM-DD" action={action} />
-      <SnackbarContent message="YYYY-MM-DD" action={action} />
-      <SnackbarContent message={"YYYY-MM-DD"} action={action} />
-      <SnackbarContent message={"YYYY-MM-DD"} action={action} />
-      <SnackbarContent message={"YYYY-MM-DD"} action={action} />
-      <SnackbarContent message={"YYYY-MM-DD"} action={action} />
-      <SnackbarContent message={"YYYY-MM-DD"} action={action} />
-      <SnackbarContent message={"YYYY-MM-DD"} action={action} />
-      <SnackbarContent message={"YYYY-MM-DD"} action={action} />
-      <SnackbarContent message={"YYYY-MM-DD"} action={action} />
-    </Stack>
-  );
+  const [data, setData] = useState([]);
+  const fetchData = () => {
+    const url = `https://cors-anywhere.herokuapp.com/http://112.168.85.171:8000/dashboard/`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((responseData) => {
+        setData(responseData.RecentReport.Reports);
+      });
+  };
+  fetchData();
+  console.log(data);
+  const tableRows = data.map((item, index) => {
+    <SnackbarContent
+      message={item[index].Date}
+      action={
+        <Stack spacing={2} direction="row">
+          <ButtonGroup>
+            <Button variant="contained" color="success">
+              C
+            </Button>
+            <Button variant="text" color="success">
+              {item[index].Checked}
+            </Button>
+          </ButtonGroup>
+
+          <ButtonGroup>
+            <Button variant="contained">D</Button>
+            <Button variant="text">{item[index].Detected}</Button>
+          </ButtonGroup>
+        </Stack>
+      }
+    />;
+  });
+  return <Stack spacing={2.3}>{tableRows}</Stack>;
 }
