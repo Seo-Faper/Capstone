304bb3cd5b0e02f7437b5aa7b642f01839eb712f
seo-faper <dswhdcks@gmail.com>
2022-12-09 16:46:47 +0900

    라우팅 다시 설정

--- capstone/src/pages/Scanning_Recent.js
+++ capstone/src/pages/Scanning_Recent.js
@@ -2,16 +2,16 @@ import React from "react";
 import ButtonAppBar from "../ButtonAppBar";
 import "../index.css";
 import Sidenav from "../Sidenav";
-function Scanning() {
+function Scanning_Recent() {
   return (
     <div>
       <ButtonAppBar></ButtonAppBar>
       <div className="Sidebar">
         <Sidenav></Sidenav>
-        <h1>스케닝 페이지 입니다.</h1>
+        <h1>Recent Report 페이지 입니다.</h1>
       </div>
     </div>
   );
 }
 
-export default Scanning;
+export default Scanning_Recent;
